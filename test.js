d6dbbe057fb228f34a2052d4cb893a1d09623499
hicham1se <hicham1se@gmail.com>
2024-01-01 19:25:28 -0600

    first commit

---
+++ test.js
@@ -0,0 +1,23 @@
+// let's start with a variable that stores numbers
+// ===============================================
+
+let numberOfCats = 2;
+numberOfCats += 5;
+let numberOfDogs = 1;
+numberOfDogs --;
+
+let totalNumberOfPets = numberOfCats + numberOfDogs;
+
+
+// keeping the practice with variables, but, now we will use another type called *constant = const*
+// ================================================================================================
+
+const hoursPerDay = 24;
+const minutesPerHour = 60;
+const secondsPerMinute = 60;
+
+const secondsInADay = hoursPerDay * minutesPerHour * secondsPerMinute;
+
+
+console.log(totalNumberOfPets);
+console.log(secondsInADay);
