93fa68cb5873208eee8edfe6409c85b12e52d0c8
hicham1se <hicham1se@gmail.com>
2024-01-02 12:38:09 -0600

    adding some strings to the song

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
-// let's start with a variable that stores numbers
-// ===============================================
+// ===========================================================
+// let's start with a variable that stores numbers and strings
+// ===========================================================
 
 let numberOfCats = 2;
 numberOfCats += 5;
 let numberOfDogs = 1;
 numberOfDogs --;
 
+let totalNumberOfPetsText = "Total number of pets is:";
 let totalNumberOfPets = numberOfCats + numberOfDogs;
 
 
+console.log(totalNumberOfPetsText,totalNumberOfPets);
+
+// ================================================================================================
 // keeping the practice with variables, but, now we will use another type called *constant = const*
 // ================================================================================================
 
@@ -16,8 +21,7 @@ const hoursPerDay = 24;
 const minutesPerHour = 60;
 const secondsPerMinute = 60;
 
+let numberOfsecondsInADayText = "Number of seconds in a day is:"
 const secondsInADay = hoursPerDay * minutesPerHour * secondsPerMinute;
 
-
-console.log(totalNumberOfPets);
-console.log(secondsInADay);
+console.log(numberOfsecondsInADayText,secondsInADay);
